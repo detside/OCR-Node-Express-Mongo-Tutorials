b40577c35edee1fcb96c84d079b074c5fa637e8c
Simon de Timary <simon.de.timary@bjss.com>
2019-01-02 14:19:56 +0000

    first commit

---
+++ backend/app.js
@@ -0,0 +1,127 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Recipe = require('./models/recipe');
+
+const app = express();
+mongoose.connect('mongodb+srv://Simon:p6JDTpJBhSZkEwb0@cluster0-x5jqf.mongodb.net/test?retryWrites=true')
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  });
+
+//the lines below are required to enable cross origin requests for all methods and all origins
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+//this is required in order to be able to read the json body object
+app.use(bodyParser.json());
+
+
+//post request to add a new recipe to the database
+app.post('/api/recipes', (req, res, next) => {
+  const recipe = new Recipe({
+    title: req.body.title,
+    ingredients: req.body.ingredients,
+    instructions: req.body.instructions,
+    difficulty: req.body.difficulty,
+    time: req.body.time
+  });
+  recipe.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Recipe saved successfully'
+      })
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      })
+    }
+  );
+});
+
+//get request to retrieve selected recipe from the database
+app.use('/api/recipes/:id', (req, res, next) => {
+  Recipe.findOne({
+    _id: req.params.id
+  }).then(
+    (thing) => {
+      res.status(200).json(thing);
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+//put request to modify a selected recipe
+app.put('/api/recipes/:id', (req, res, next) => {
+  const recipe = new Recipe({
+    _id: req.params.id,
+    title: req.body.id,
+    ingredients: req.body.ingredients,
+    instructions: req.body.instructions,
+    difficulty: req.body.difficulty,
+    time: req.body.time
+  });
+  Recipe.updateOne({_id: req.params.id}, thing).then(
+    () => {
+      res.status(201).json({
+        message: "Recipe updated successfully"
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+//delete selected recipe
+app.delete('/api/recipes/:id', (req, res, next) => {
+  Recipe.deleteOne({_id: req.params.id}).then(
+    () => {
+      res.status(200).json({
+        message: "Recipe deleted successfully"
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+//get request to retrieve all recipes in the database
+app.use('/api/recipes', (req, res, next) => {
+  Recipe.find().then(
+    (recipes) => {
+      res.status(200).json(recipes);
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+module.exports = app;
